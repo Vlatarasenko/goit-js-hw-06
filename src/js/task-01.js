53b6061c3156702bf894a79876f6e22141e9ee13
Vlatarasenko <vlatarasenko@gmail.com>
2022-11-02 19:00:03 +0200

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+// кількість категорій
+const arrayItem = categories.querySelectorAll("li.item");
+console.log(`Number of categories: ${arrayItem.length}`);
+// Текст h2 та кільіксть елементів
+arrayItem.forEach((element) => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.querySelectorAll("li").length}`);
+});
